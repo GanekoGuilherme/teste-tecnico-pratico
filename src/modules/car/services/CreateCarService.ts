fa654be087b579419294f10f96c327ee01d16be6
GanekoGuilherme <guilhermeganeko@hotmail.com>
2022-07-14 01:20:54 -0300

    feat: implementing CreateCarService.

--- src/modules/car/services/CreateCarService.ts
+++ src/modules/car/services/CreateCarService.ts
@@ -1,3 +1,37 @@
-// export default class CreateCarService {
-//   constructor(private carRepository: ICarRepository);
-// }
+import ICarRepository, {
+  ICreateCarDTO,
+} from "@shared/infra/database/mongoose/repositories/CarRepository/models/ICarRepository";
+import { ICarDTO } from "@shared/infra/database/mongoose/repositories/CarRepository/schemas/Car";
+import { inject, injectable } from "tsyringe";
+
+interface IResponseDTO {
+  car: ICarDTO;
+}
+
+@injectable()
+export default class CreateCarService {
+  constructor(@inject("CarRepository") private carRepository: ICarRepository) {}
+
+  async execute({
+    licensePlate,
+    color,
+    brand,
+  }: ICreateCarDTO): Promise<IResponseDTO> {
+    const carAlreadyExists = await this.carRepository.findCarByLicensePlate(
+      licensePlate
+    );
+
+    if (!carAlreadyExists) {
+      //TODO criar class AppError
+      throw new Error("License Plate already is unavailable.");
+    }
+
+    const car = await this.carRepository.createCar({
+      licensePlate,
+      color,
+      brand,
+    });
+
+    return { car };
+  }
+}
